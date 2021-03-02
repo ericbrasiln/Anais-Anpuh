a24e03e0c08f39c5d27fb64d87e5d63fde93b5a8
Vitor Mussa <vtrmussa@gmail.com>
2021-03-02 19:59:23 -0300

    Inicia pythonização, modularização e corrige detalhes

--- script-anais-anpuh.py
+++ script-anais-anpuh.py
@@ -1,50 +1,77 @@
-from urllib.request import Request, urlopen, urlretrieve
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-from urllib import request
+import pandas as pd
 import re
 import os
-import urllib
-import pandas as pd
-import requests
 import wget
 
-dicionario = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
-urlbase = 'https://anpuh.org.br'
-url = 'https://anpuh.org.br/index.php/documentos/anais'
+# Define parâmetros para os pedidos HTTP
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+BASE_URL = 'https://anpuh.org.br'
+URL = 'https://anpuh.org.br/index.php/documentos/anais'
 
-listaFinal =[]
-linkAnterior = ""
+
+# Cria funções auxiliares
+def get_soup(url, headers=HEADERS):
+    """Obtém objeto BeautifulSoup (soup) para o url dado."""
+    reqopen = Request(url, headers=headers)
+    req = urlopen(reqopen)
+    soup = BeautifulSoup(req.read(), "html.parser")
+    return soup
+
+
+def make_new_folder(*paths):
+    """Cria diretório caso ele ainda não exista e retorna o path
+    correspondente."""
+    folder_path = os.path.join(*paths)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    return folder_path
+
+
+def get_events_urls():
+    """Raspa URLs de todos os eventos da ANPUH."""
+    pass
+
+
+def get_event_number(event_url):
+    """Extrai número do evento com base em seu URL."""
+    pass
+
+
+# Cria variáveis auxiliares
+lista_final = []
+link_anterior = ""
+
 # Acessa a página inicial dos Anais.
-reqopen = Request(url, headers=dicionario)
-req = urlopen(reqopen)
-bs = BeautifulSoup(req.read(), 'lxml')
+soup_home = get_soup(URL)
+
 # Define e cria a pasta para salvar cada evento.
 print('Criando pasta de salvamento...')
-pasta = os.path.join('Anais  Anpuh', 'pdf')
-if not os.path.exists(pasta):
-    os.makedirs(pasta)
-#Define os links para cada evento.
+pasta = make_new_folder('Anais Anpuh', 'pdf')
+
+# Define os links para cada evento.
 print('Criando a lista de eventos a partir da página principal...')
-boxAnais = bs.find(id='cobalt-section-1')
-links = boxAnais.find_all('a', href=re.compile(r'(1-anais-simposios-anpuh/)'))
-for linkAnais in links:
+box_anais = soup_home.find(id='cobalt-section-1')
+links = box_anais.find_all('a', href=re.compile(r'(1-anais-simposios-anpuh/)'))
+
+for link_anais in links:
     acabou = False
-    linkEvento = linkAnais['href']     
-    linkEventoFinal = urlbase + linkEvento    
-    numEvento = linkEvento.replace('index.php/documentos/anais/category-items/1-anais-simposios-anpuh/','')    
+    link_evento = link_anais['href']     
+    link_evento_final = BASE_URL + link_evento    
+    id_evento = link_evento.replace('index.php/documentos/anais/category-items/1-anais-simposios-anpuh/','')[1:]    
+    
     # Acessa as páginas de cada evento e raspa os pdfs.
     while acabou == False:
-        response = request.urlopen(linkEventoFinal).read()
-        soup= BeautifulSoup(response, "html.parser")             
+        soup_eventos = get_soup(link_evento_final)
+        
         #Encontra a caixa com os papers
-        paperBoxes = soup.find_all(class_='has-context')
+        paper_boxes = soup_eventos.find_all(class_='has-context')
         print('Encontrando todos os papers da página...')
-        pastaEvento = os.path.join(pasta, numEvento[1:])
-        print('Encontrando todos as informações dos papers da página...')
-            
-        if not os.path.exists(pastaEvento):
-            os.makedirs(pastaEvento)
-        for paper in paperBoxes:    
+        pasta_evento = make_new_folder(pasta, id_evento)
+        print('Encontrando todos as informações dos papers da página...')    
+        
+        for paper in paper_boxes:    
             # Encontra os títulos de cada paper.
             title = paper.h2.text
             title = title.strip().lower().replace('/','-')
@@ -54,7 +81,8 @@ for linkAnais in links:
             evento = ""
             ano = ""
             autores = ""
-            linkArquivo = ""
+            link_arquivo = ""
+
             for informacao in informacoes:
                 if informacao.text.strip() == "Tipo":
                     tipo = informacao.find_next_sibling().text.strip()
@@ -67,67 +95,72 @@ for linkAnais in links:
                     print (f"Ano : {ano}")
                 if informacao.text.strip() == "Arquivo":
                     arquivo = informacao.find_next_sibling().a['href']
-                    linkArquivo = urlbase + arquivo
-                    print (f"Arquivo : {linkArquivo}")
+                    link_arquivo = BASE_URL + arquivo
+                    print (f"Arquivo : {link_arquivo}")
                 if informacao.text.strip() == "PDF LINK":
                     arquivo = informacao.find_next_sibling().a['href']
                     if arquivo.startswith('https://') == True:
-                        linkArquivo = arquivo
-                        print (f"Arquivo : {linkArquivo}")
+                        link_arquivo = arquivo
+                        print (f"Arquivo : {link_arquivo}")
                     else:
-                        linkArquivo = urlbase + arquivo
-                        print (f"Arquivo : {linkArquivo}")
+                        link_arquivo = BASE_URL + arquivo
+                        print (f"Arquivo : {link_arquivo}")
                 if informacao.text.strip() == "Autor(es)":
                     autores = informacao.find_next_sibling().text.strip()
                     print (f"Autor(es) : {autores}")
-            listaInterna = [autores, title, tipo, evento, ano, linkArquivo]
-            listaFinal.append(listaInterna)
+            
+            lista_interna = [autores, title, tipo, evento, ano, link_arquivo]
+            lista_final.append(lista_interna)
 
             # Encontra os links para os pdfs.
             print('Encontrando link do paper...')
             try:
-                arquivolink = paper.find('a',href=re.compile(r'(.pdf)'))
-                link = arquivolink['href']
-                if not link in linkAnterior:
-                    linkAnterior = link
+                arquivo_link = paper.find('a',href=re.compile(r'(.pdf)'))
+                link = arquivo_link['href']
+                if not link in link_anterior:
+                    link_anterior = link
                     if link.startswith('https://'):
-                        fullLink = link
-                        fullName = os.path.join(pastaEvento, title.replace(' ','_') + '.pdf')
+                        full_link = link
+                        full_name = os.path.join(pasta_evento, title.replace(' ','_') + '.pdf')
                     else:
-                        fullLink = "https://anpuh.org.br" + link            
+                        full_link = "https://anpuh.org.br" + link            
                         alterar = ['"', '*', ':', '<', '>', '?', '/', "\\", '|' , '_' , '@' , '+', '...']
                         for x in alterar:
                             title = title.replace(x, '')
-                        fullName = os.path.join(pastaEvento, title.replace(' ','_') + '.pdf')
-                    if not os.path.exists(fullName):
+                        full_name = os.path.join(pasta_evento, title.replace(' ','_') + '.pdf')
+                    if not os.path.exists(full_name):
                         print('Salvando o pdf na pasta...\n')
                         try:
-                            #request.urlretrieve(fullLink, fullName)
-                            wget.download(fullLink, out=fullName)
+                            wget.download(full_link, out=full_name)
                         except Exception as e:
                             print(e)
                     else:
                         print("Arquivo já existe.")
                 else:
                     print("PDF desse paper é igual ao do paper anterior.")
+            
             except Exception as e:
-                print (e)
+                print(e)
                 link = paper.find('a', href=re.compile(r'(.pdf)'))
                 link = None
                 print('Paper sem pdf disponível para download.')
+        
         # Busca a próxima página de papers.
-        menuFinal = soup.find(class_='pagination')
+        menu_final = soup_eventos.find(class_='pagination')
         print('Procurando pŕoxima página...')
+
         try:
-            proximaPag = menuFinal.find('a', title=re.compile(r'(Próx)'))
-            linkProximaPag = proximaPag['href']
-            print(numEvento)
-            print('Página encontrada: https://anpuh.org.br' + linkProximaPag)
-            linkEventoFinal = 'https://anpuh.org.br' + linkProximaPag
+            proxima_pag = menu_final.find('a', title=re.compile(r'(Próx)'))
+            link_proxima_pag = proxima_pag['href']
+            print(id_evento)
+            print('Página encontrada: https://anpuh.org.br' + link_proxima_pag)
+            link_evento_final = 'https://anpuh.org.br' + link_proxima_pag
         except:                        
             print("Final das Páginas de Papers desse evento.")
             acabou = True
+
+# Exporta CSV com informações gerais dos papers.
 print('Salvando arquivo .csv com todas as informações: autores/instituições, título, tipo, evento, ano, link do pdf')
-df = pd.DataFrame(listaFinal, columns=['Autor(es)/Instituições', 'Título', 'Tipo', 'Evento', 'Ano', 'Link do Arquivo'])
+df = pd.DataFrame(lista_final, columns=['Autor(es)/Instituições', 'Título', 'Tipo', 'Evento', 'Ano', 'Link do Arquivo'])
 df.to_csv('anais-anpuh-infos.csv')
 print('Raspagem completa.')
